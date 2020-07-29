731f7df1e2ce5a4128615d8c6cb79d676b4c4cb5
Carlos Fernando Barcos Sinche <fbarcos8@hotmail.com>
2020-07-28 23:34:24 -0500

    Se agregan inputs: 	- textfield 	- email 	- password 	- datepicker, con localization
    	modificado:     data/menu_opts.json
    	modificado:     lib/main.dart
    	nuevo archivo:  lib/src/pages/input_page.dart
    	modificado:     lib/src/routes/routes.dart
    	modificado:     lib/src/utils/icono_string_util.dart
    	modificado:     pubspec.lock
    	modificado:     pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 
 void main() => runApp(MyApp());
@@ -8,6 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'), // Hebrew, no country code
+      ],
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       //home: HomePage(),
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/animated_container_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage(),
   };
 }
